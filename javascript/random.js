29f490ef39affcf11323de2a20c5229679af9a0c
GabrielRF1 <rasferreiramilde@gmail.com>
2021-07-04 17:46:45 -0300

    Park-Miller

--- javascript/random.js
+++ javascript/random.js
@@ -6,8 +6,29 @@ function gcd(a, b) {
   return gcd(b, a % b);
 }
 
+function renderResult(binaryString, bits, id) {
+  var decString = (BigInt(binaryString)).toString();
+  var binString = binaryString.slice(2);
+  document.getElementById("randResult(decimal)"+id).innerHTML = "";
+  document.getElementById("randResult(bin)"+id).innerHTML = "";
+
+  for (var i = 0; i < decString.length; i++) {
+    document.getElementById("randResult(decimal)"+id).innerHTML += decString[i];
+    if ((i+1) % 50 == 0)
+      document.getElementById("randResult(decimal)"+id).innerHTML += "<br>";
+  }
+
+  for (var i = 0; i < binString.length; i++) {
+    document.getElementById("randResult(bin)"+id).innerHTML  += binString[i];
+    if ((i+1) % 50 == 0)
+      document.getElementById("randResult(bin)"+id).innerHTML  += "<br>";
+  }
+
+  document.getElementById("randResultBits"+id).innerHTML = bits;
+  document.getElementById("randResultExec"+id).innerHTML = "verificar console do navegador";
+}
+
 function blumBlumShub(bits) {
-  //var t0 = performance.now();
   console.time('blumBlumShub');
 
   if (bits <= 0) {
@@ -20,7 +41,7 @@ function blumBlumShub(bits) {
   let m = p*q;    // "M = pq is the product of two large primes p and q." - wikipedia
 
   let seed = Number(new Date());  // Seed: tempo atual em milisegundos desde 01.01.1970
-  while (gcd(seed, m) != 1) {     // Se M e o Seed sao co-primos, redesignar seed
+  while (gcd(seed, m) != 1) {     // Se M e o Seed nao sao co-primos, redesignar seed
     seed = Number(new Date());
   }
 
@@ -28,15 +49,38 @@ function blumBlumShub(bits) {
                               // da string para o número ser entendida como binário -> decimal
   let xi = seed;
   for (let i = 0; i < bits; i++) {
-    xi = (xi*xi) % m;        // x(n-1) = (x(n))^2 mod M
+    xi = (xi*xi) % m;        // x(n+1) = (x(n))^2 mod M
     binaryString += (xi%2);  // concatena resto da divisão por dois na string
   }
 
-  //var time = performance.now() - t0;
   console.timeEnd('blumBlumShub');
-  document.getElementById("randResult(decimal)").innerHTML = BigInt(binaryString);
-  document.getElementById("randResult(bin)").innerHTML = binaryString.slice(2);
-  document.getElementById("randResultBits").innerHTML = bits;
-  document.getElementById("randResultExec").innerHTML = "verificar console do navegador";//(time).toPrecision(50) + "ms";
-  //return {binStr:binaryString.slice(2), dec:Number(binaryString), exec:(tf-t0)};
+  renderResult(binaryString, bits, "1");
+}
+
+// Park–Miller random number generator
+function parkMiller(bits) {
+  console.time('parkMiller');
+  if (bits <= 0) {
+    alert("Entre um tamanho de bits")
+    return ;
+  }
+
+  let m = Math.pow(2, 31) - 1; // Valore propostos por Park e Miller
+  let a = Math.pow(7, 5)       // M -> Primo Mersenne; a -> raiz primitiva módulo M
+
+  let seed = Number(new Date());  // Seed: tempo atual em milisegundos desde 01.01.1970
+  while (gcd(seed, m) != 1) {     // Se M e o Seed nao sao co-primos, redesignar seed
+    seed = Number(new Date());
+  }
+
+  let binaryString = "0b";    // Coloca-se 0b no começo para a conversão
+                              // da string para o número ser entendida como binário -> decimal
+  let xi = seed;
+  for (let i = 0; i < bits; i++) {
+    xi = (a*xi) % m;         // x(n+1) = (a*x(n)) mod M
+    binaryString += (xi%2);  // concatena resto da divisão por dois na string
+  }
+
+  console.timeEnd('parkMiller');
+  renderResult(binaryString, bits, "2");
 }
