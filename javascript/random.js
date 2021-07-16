032d2cda3850d3318330623dd4046fe9f89384dc
GabrielRF1 <rasferreiramilde@gmail.com>
2021-07-16 10:25:50 -0300

    Friendship with park-miller ended. LCG is my new best friend

--- javascript/random.js
+++ javascript/random.js
@@ -36,11 +36,12 @@ function blumBlumShub(bits) {
     return ;
   }
 
-  let p = 4157;   // p e q -> primos grandes (escolhidos manualmente,
-  let q = 4337;   // sem critério, de uma tabela da wikipedia)
+  let p = 781;   // p e q -> primos grandes congruentes a 3(mod 4)
+  let q = 789;   // escolhidos os dois maiores desssa lista https://oeis.org/A016105
   let m = p*q;    // "M = pq is the product of two large primes p and q." - wikipedia
 
   let seed = Number(new Date());  // Seed: tempo atual em milisegundos desde 01.01.1970
+
   while (gcd(seed, m) != 1) {     // Se M e o Seed nao sao co-primos, redesignar seed
     seed = Number(new Date());
   }
@@ -53,34 +54,41 @@ function blumBlumShub(bits) {
     binaryString += (xi%2);  // concatena resto da divisão por dois na string
   }
 
+
   console.timeEnd('blumBlumShub');
   renderResult(binaryString, bits, "1");
 }
 
-// Park–Miller random number generator
-function parkMiller(bits) {
-  console.time('parkMiller');
+var seed = BigInt(0);
+
+// LCG random number generator c = 0, M power of 2
+function LCG(bits) {
+  console.time('LCG');
+
   if (bits <= 0) {
     alert("Entre um tamanho de bits")
     return ;
   }
 
-  let m = Math.pow(2, 31) - 1; // Valore propostos por Park e Miller
-  let a = Math.pow(7, 5)       // M -> Primo Mersenne; a -> raiz primitiva módulo M
+  let m = BigInt(Math.pow(2,bits)); // M potencia de 2
+  let a = BigInt(541);              // congruente a 5(mod 8)
+  let c = BigInt(0);                // c = 0
 
-  let seed = Number(new Date());  // Seed: tempo atual em milisegundos desde 01.01.1970
-  while (gcd(seed, m) != 1) {     // Se M e o Seed nao sao co-primos, redesignar seed
-    seed = Number(new Date());
+  if (seed == BigInt(0)) {
+    seed = BigInt(new Date()) | BigInt(0b1); // Deve ser impar
+
+    while (gcd(seed, m) != 1) {     // Se M e o Seed nao sao co-primos, redesignar seed
+      seed = BigInt(new Date()) | BigInt(0b1);
+    }
   }
-
-  let binaryString = "0b";    // Coloca-se 0b no começo para a conversão
-                              // da string para o número ser entendida como binário -> decimal
   let xi = seed;
-  for (let i = 0; i < bits; i++) {
-    xi = (a*xi) % m;         // x(n+1) = (a*x(n)) mod M
-    binaryString += (xi%2);  // concatena resto da divisão por dois na string
+  for (var i = 0; i < 500; i++) {
+    xi = (a*xi + c) % m;         // x(n+1) = (a*x(n) + c) mod M
+    if (xi.toString(2).length == bits)
+      break;
   }
 
-  console.timeEnd('parkMiller');
-  renderResult(binaryString, bits, "2");
+  seed = xi;
+  console.timeEnd('LCG');
+  renderResult("0b"+xi.toString(2), xi.toString(2).length, "2");
 }
